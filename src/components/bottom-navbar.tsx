689cfee18ac95ba617c9e26420e9606b8e01b7a3
Infinite-Concept <ifenowoifesegun@gmail.com>
2025-08-19 21:09:59 +0100

    Update UI component

--- src/components/bottom-navbar.tsx
+++ src/components/bottom-navbar.tsx
@@ -11,6 +11,7 @@ import MenuContext from "@/lib/context/MenuContext";
 import MenuProvider from "@/lib/context/MenuContext";
 import OpenMobileMenuBtn from "./OpenMobileMenuBtn";
 import { usePathname } from "next/navigation";
+import AnyaEyeIcon from "./icons/AnyaEyeIcon";
 
 interface BottomNavBarItemProps {
   imgUrl?: string;
@@ -31,15 +32,20 @@ export default function BottomNavBar() {
       route: "/",
       name: "Home"
     },
-    {
-      imgUrl: "/icons/icons8-search-2.png",
-      route: "/search",
-      name: "Search"
-    },
+    // {
+    //   imgUrl: "/icons/icons8-search-2.png",
+    //   route: "/",
+    //   name: "Search"
+    // },
     {
       imgUrl: "/icons/icons8-notification-1.png",
       route: "/notifications",
       name: "Notifications"
+    },
+    {
+      icon: <AnyaEyeIcon active={pathname === "/search"} />,
+      route: "/anya",
+      name: "",
     },
     {
       imgUrl: "/icons/icons8-message.png",
@@ -57,14 +63,17 @@ export default function BottomNavBar() {
         <div className="w-[25rem] mx-auto max-[600px]:w-[90%] rounded-2xl px-4 pt-4 pb-2 bg-gray-900 flex justify-between max-[500px]:w-[93%] bottom-4">
           {routes.map((item, i) => (
             <Link key={i} href={item.route} className={`w-12 flex flex-col items-center  group hover:scale-110 transition-all duration-500`}>
-              {<Image
+              {item.icon ? (
+                <div className="">{item.icon}</div>
+              ) : (
+                <Image
                   src={item.imgUrl || ""}
                   className={`size-8 grayscale ${pathname === item.route ? "grayscale-0" : ""}`}
-                  alt="Home"
+                  alt={item.name || "icon"}
                   width={100}
                   height={100}
                 />
-              }
+              )}
               <p className={` group-hover:opacity-100 opacity-0 mt-1 ${pathname === item.route ? "text-white" : "text-gray-500 "} text-xs transition-all duration-300`}>{item.name}</p>
             </Link>
           ))}
