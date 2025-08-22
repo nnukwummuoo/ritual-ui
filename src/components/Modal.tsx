d758f22af64cc562bd5177ebca35d083af3f2cb8
Kingsley Toochi <tochukwuk68@gmail.com>
2025-08-22 13:01:26 +0100

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,16 +9,8 @@ const Modal = ({ show, message }: ModalProps) => {
   if (!show) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex md:items-center md:justify-start items-end justify-center w-full z-50">
-      <div
-        className="
-          p-6 bg-white rounded-t-2xl md:rounded-lg shadow-lg md:ml-40 
-          w-full md:w-4/5 
-          transform transition-transform duration-300 ease-out
-          translate-y-0
-          animate-slideUp
-        "
-      >
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center md:justify-start w-[90%] mx-auto md:w-4/5 z-50">
+      <div className="p-6 bg-white rounded-lg shadow-lg md:ml-40">
         <p className="font-semibold text-center text-orange-500">{message}</p>
       </div>
     </div>
@@ -26,4 +18,3 @@ const Modal = ({ show, message }: ModalProps) => {
 };
 
 export default Modal;
-
