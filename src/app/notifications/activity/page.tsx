b14b7e2bd73b5b7821e7bc6fba75fdb7dbe55209
Kingsley Toochi <tochukwuk68@gmail.com>
2025-08-22 21:02:11 +0100

    Update page.tsx

--- src/app/notifications/activity/page.tsx
+++ src/app/notifications/activity/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react' 
 import { BiTimeFive } from "react-icons/bi";
 import RequestCard from '../components/RequestCard';
 
 export default function Activity() {
   return <div className='flex flex-col gap-8 max-w-[26rem] mx-auto'>
     <RequestCard type="model" img="/picture-2.jfif" status={"request"} name='Hazel Hazel' exp='23h, 4m' />
-    <RequestCard type="fan" img='/picture-1.jfif' status={"request"} name='Maxwell Dickson' titles={["Model", "Creator", "Influencial"]}  exp='23h, 4m'/>
+    <RequestCard type="fan" img='/picture-1.jfif' status={"request"} name='Maxwell Dickson' titles={["Model", "Creator", "Influencial"]}  exp='23h, 4m' schedule="2025-08-22 08:00"/>
     <RequestCard type="fan" img='/picture-1.jfif' status={"accepted"} name='Maxwell Dickson' titles={["Model", "Creator", "Influencial"]}  exp='23h, 4m'/>
     <RequestCard type="fan" img='/picture-1.jfif' status={"completed"} name='Maxwell Dickson' titles={["Model", "Creator", "Influencial"]}  exp='23h, 4m'/>
     <RequestCard type="model" img="/picture-2.jfif" status={"expired"} name='Hazel Hazel' exp='23h, 4m' />
