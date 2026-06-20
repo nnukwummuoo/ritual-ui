db8974d5efc47833cd9a15f92538e49d29f31a3b
kossye59 <kossye59@gmail.com>
2026-06-20 22:43:46 +0800

    laptop/tab resize

--- src/app/mmeko/admin/page.tsx
+++ src/app/mmeko/admin/page.tsx
@@ -357,7 +357,7 @@ useEffect(() => {
                   }`}
               >
                 {item.icon}
-                <span className="font-medium hidden 2xl:inline ml-0 2xl:ml-2">{item.name}</span>
+                <span className="font-medium hidden md:inline ml-0 md:ml-2">{item.name}</span>
                 {item.name === "Creator Verification" && docCount > 0 && (
                   <span className="ml-2 bg-red-500 text-white px-1.5 py-1 rounded-full text-xs">
                     {docCount}
