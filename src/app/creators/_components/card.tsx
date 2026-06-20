1aed65fe71b0184b6fae24c0abdf86839230f14a
kossye59 <kossye59@gmail.com>
2026-06-20 19:28:06 +0800

    laptop/tab resize

--- src/app/creators/_components/card.tsx
+++ src/app/creators/_components/card.tsx
@@ -239,12 +239,12 @@ export const CreatorCard = ({
           </div>
 
           {/* Host Type */}
-          <h4 className="px-1.5 py-1 text-[10px] md:text-[10px] 2xl:text-xs 2xl:px-2.5 2xl:py-1.5 bg-black bg-opacity-50 rounded-lg overflow-auto">
+          <h4 className="px-1.5 py-1 text-[10px] md:text-[10px] 2xl:text-xs 2xl:px-2.5 2xl:py-1.5 bg-black bg-opacity-40 rounded-lg whitespace-nowrap">
             {hosttype}
           </h4>
 
           {/* First Name */}
-          <h4 className="px-2.5 py-1.5 text-xs bg-black bg-opacity-50 rounded-lg overflow-auto">
+          <h4 className="px-1.5 py-1 text-[10px] md:text-[10px] 2xl:text-xs 2xl:px-2.5 2xl:py-1.5 bg-black bg-opacity-50 rounded-lg overflow-auto">
             {name?.split?.(" ")[0] ?? "Name"}
           </h4>
         </div>
