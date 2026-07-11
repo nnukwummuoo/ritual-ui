6146a9de334898e9027825d3f253461f638b4afd
kossye59 <kossye59@gmail.com>
2026-07-12 05:35:46 +0800

    update

--- src/app/creators/create-creator/page.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/creators/create-creator/page.tsx
-"use client";
-import CreateCreatorPortfolio from "./CreateCreatorPortfolio";
-
-export default function Page() {
-  return <CreateCreatorPortfolio />;
-}
